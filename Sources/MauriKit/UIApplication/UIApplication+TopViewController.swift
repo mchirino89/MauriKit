f232ab10870624fe61ae9e168136b5924ffcd532
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2021-04-26 19:29:43 -0300

    Add helper to locate top view controller in any stack

--- Sources/MauriKit/UIApplication/UIApplication+TopViewController.swift
+++ Sources/MauriKit/UIApplication/UIApplication+TopViewController.swift
@@ -1,8 +1,31 @@
 //
-//  File.swift
+//  UIApplication+TopViewController.swift
 //  
 //
 //  Created by Mauricio Chirino on 26/4/21.
 //
 
-import Foundation
+import UIKit
+
+public extension UIApplication {
+    /// Fetches for the top view controller from a given root controller in any stack
+    /// - Parameter rootController: controller from where recursive search will begin. It may even container controllers
+    ///  such as `UINavigationController` or `UITabBarController`
+    /// - Returns: `UIViewController` on top of the stack, should it exist.
+    class func topViewController(from rootController: UIViewController?) -> UIViewController? {
+        if let navigationController = rootController as? UINavigationController {
+            return topViewController(from: navigationController.visibleViewController)
+        }
+
+        if let tabController = rootController as? UITabBarController,
+           let selected = tabController.selectedViewController {
+            return topViewController(from: selected)
+        }
+
+        if let presented = rootController?.presentedViewController {
+            return topViewController(from: presented)
+        }
+
+        return rootController
+    }
+}
